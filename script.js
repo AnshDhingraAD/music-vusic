c273fc512f39aaff8d0fc8991350e582d14c1033
AnshDhingraAD <anshdhingra6827@gmail.com>
2025-04-09 01:02:58 +0530

    js commit

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ let currFolder;
 let currentaudio=new Audio();
 async function fetchsongs(folder) {
     currFolder=folder
-    let a = await fetch(`${currFolder}/`)
+    let a = await fetch(`/public/${currFolder}/`)
     let response = await a.text();
     console.log(response)
     let div = document.createElement('div')
