57b8ba8d9d35f7b9adc16b74dd19083d0ed30345
AnshDhingraAD <162344929+AnshDhingraAD@users.noreply.github.com>
2025-04-08 20:51:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -55,7 +55,7 @@ function formatTime(seconds) {
 }
 
 async function displayalbum(){
-    let a = await fetch(`http://127.0.0.1:5500/songs/`)
+    let a = await fetch(`/songs/`)
     let response = await a.text();
     console.log(response)
     let div = document.createElement('div')
